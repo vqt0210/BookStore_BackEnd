4a4c17cbeb23425e27406d7583238a04cdad74d7
VQT <quoctuan02101@gmail.com>
2024-11-17 04:13:26 +0700

    index & vercel update

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 
-// middleware
+const app = express();
+
+// Middleware
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-store-front-end-tawny.vercel.app'],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://book-store-front-end-tawny.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
-// routes
-const bookRoutes = require('./src/books/book.route');
-const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
-const adminRoutes = require("./src/stats/admin.stats")
+// Routes
+const bookRoutes = require("./src/books/book.route");
+const orderRoutes = require("./src/orders/order.route");
+const userRoutes = require("./src/users/user.route");
+const adminRoutes = require("./src/stats/admin.stats");
 
-app.use("/api/books", bookRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/auth", userRoutes)
-app.use("/api/admin", adminRoutes)
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminRoutes);
 
-async function main() {
-  await mongoose.connect(process.env.DB_URL);
-  app.use("/", (req, res) => {
-    res.send("Book Store Server is running!");
-  });
-}
-
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Base route
+app.use("/", (req, res) => {
+  res.send("Book Store Server is running!");
 });
+
+// Database connection
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch((err) => console.log(err));
+
+// Export Express app for serverless deployment
+module.exports = app;
