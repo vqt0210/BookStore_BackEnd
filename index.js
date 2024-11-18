870d7eb1d2fd5f2887dee048e292e1244d761735
VQT <quoctuan02101@gmail.com>
2024-11-18 15:36:04 +0700

    add new vercel to index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:5173",
       "https://book-store-front-end-tawny.vercel.app",
       "https://www.teasonmike.io.vn",
+      "https://book-store-front-end-git-main-vqtees-projects.vercel.app/",
     ],
     credentials: true,
   })
