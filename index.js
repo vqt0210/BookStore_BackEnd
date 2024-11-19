01a4c66b4bdcdcaf8aefb6d284f522ec4614feb2
Quốc Tuấn <quoctuan02101@gmail.com>
2024-11-19 17:00:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://book-store-front-end-tawny.vercel.app",
+      "https://book-store-front-end-ivory.vercel.app",
       "https://www.teasonmike.io.vn",
       "https://book-store-front-end-git-main-vqtees-projects.vercel.app/",
     ],
